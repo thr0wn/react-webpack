52cc9802a68e59896500ee67f4d5331651616ef6
Natan Camargos <natanscamargos@gmail.com>
2025-10-27 16:48:46 -0300

    cleaning files not needed anymore

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,17 +1,13 @@
 import React, { useEffect } from "react";
 import Header from "@/src/components/Header"
-// import Hero from "@/src/components/Hero";
-// import Footer from "@/src/components/Footer";
 import "./index.css";
 
 const Home = () => (
   <main className="home-page">
     <Header />
     <article className="home-content">
-      {/* <Hero /> */}
-      ...
+      home content
     </article>
-  {/* <Footer /> */}
   </main>
 );
 
