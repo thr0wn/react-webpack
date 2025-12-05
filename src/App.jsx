0bab307cd95e0198f0651fa4564f4a649bf8ef15
Natan Camargos <natanscamargos@gmail.com>
2025-12-05 08:59:27 -0300

    remove stackoverflow issue

--- src/App.jsx
+++ src/App.jsx
@@ -3,27 +3,20 @@ import { Redirect, Route, Switch } from "react-router-dom";
 import Home from "@/src/pages/Home";
 import About from "@/src/pages/About";
 import PageWithModal from "@/src/pages/PageWithModal";
-import { UsernameContext } from "@/src/components/UsernameModal";
 
-const App = () => {
-  const [usernameContext, setUsernameContext] = useState({ open: false, resolve: () => null, setContext: () => null });
-
-  return (
-    <UsernameContext.Provider value={{ ...usernameContext, setContext: setUsernameContext }}>
-      <Switch>
-        <Route path="/home">
-          <Home />
-        </Route>
-        <Route path="/about">
-          <About />
-        </Route>
-        <Route path="/page-with-modal">
-          <PageWithModal />
-        </Route>
-        <Redirect from="/*" to="/home" exact />
-      </Switch>
-    </UsernameContext.Provider>
-  );
-};
+const App = () => (
+  <Switch>
+    <Route path="/home">
+      <Home />
+    </Route>
+    <Route path="/about">
+      <About />
+    </Route>
+    <Route path="/page-with-modal">
+      <PageWithModal />
+    </Route>
+    <Redirect from="/*" to="/home" exact />
+  </Switch>
+);
 
 export default App;
